2005c11bca5df8d7706bba895c20940ccb8b9ae7
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-03-17 08:12:14 +0530

    Day01

--- src/day01/Program1.java
+++ src/day01/Program1.java
@@ -4,13 +4,14 @@ package day01;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class Program1 {
 
 	public static void main(String[] args) {
 	 WebDriver driver;	
 	 
-	 driver = new ChromeDriver();
+	 driver = new EdgeDriver();
 	 driver.get("https://demo.automationtesting.in/Register.html");	
 	 driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Test");
 	 driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("Java");
